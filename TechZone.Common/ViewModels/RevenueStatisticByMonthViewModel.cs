498aefd1cb28bfa2ce7a75d5fc51516eab1e1f61
Dau Ngoc Hung <hungdau192@gmail.com>
2023-04-29 23:17:14 +0700

    update thong ke

--- TechZone.Common/ViewModels/RevenueStatisticByMonthViewModel.cs
+++ TechZone.Common/ViewModels/RevenueStatisticByMonthViewModel.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace TechZone.Common.ViewModels
 {
-    public class RevenueStatisticViewModel
+    public class RevenueStatisticByMonthViewModel
     {
-        public DateTime Date { set; get; }
+        public int Month { set; get; }
         public decimal Revenues { set; get; }
         public decimal Benefit { set; get; }
     }
